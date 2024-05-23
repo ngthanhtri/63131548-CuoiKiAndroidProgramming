765741355bab044d73edacac3daa1d625b1d9e99
thanhtri2112 <thanhtri21120309@gmail.com>
2024-05-23 19:33:49 +0700

    code java DangNhapOTP và DangNhapSDT

--- Chat App/app/src/main/java/thanhtri63131548/ntuedu/chatapp/DangNhapOTP.java
+++ Chat App/app/src/main/java/thanhtri63131548/ntuedu/chatapp/DangNhapOTP.java
@@ -3,12 +3,25 @@ package thanhtri63131548.ntuedu.chatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DangNhapOTP extends AppCompatActivity {
 
+    String sdt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dangnhapotp);
+
+        sdt = getIntent().getExtras().getString("dt");
+        Toast.makeText(getApplication(),sdt, Toast.LENGTH_LONG).show();
+
+
     }
 }
--- Chat App/app/src/main/java/thanhtri63131548/ntuedu/chatapp/DangNhapSDT.java
+++ Chat App/app/src/main/java/thanhtri63131548/ntuedu/chatapp/DangNhapSDT.java
@@ -2,13 +2,42 @@ package thanhtri63131548.ntuedu.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.hbb20.CountryCodePicker;
 
 public class DangNhapSDT extends AppCompatActivity {
 
+    CountryCodePicker maquocgia;
+    EditText sdtInput;
+    Button btnGuiOTP;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dangnhapsdt);
+
+        maquocgia = findViewById(R.id.login_countrycode);
+        sdtInput = findViewById(R.id.login_mobile_number);
+        btnGuiOTP = findViewById(R.id.send_otp_btn);
+        progressBar = findViewById(R.id.login_progress_bar);
+
+        progressBar.setVisibility(View.GONE);
+
+        maquocgia.registerCarrierNumberEditText(sdtInput);
+        btnGuiOTP.setOnClickListener((v)-> {
+            if(!maquocgia.isValidFullNumber()){
+                sdtInput.setError("Số điện thoại không đúng");
+                return;
+            }
+            Intent intent = new Intent(DangNhapSDT.this,DangNhapOTP.class);
+            intent.putExtra("dt",maquocgia.getFullNumberWithPlus());
+            startActivity(intent);
+        });
     }
 }
