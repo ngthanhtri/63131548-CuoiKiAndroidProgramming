85d9eed14b8b6abf79fc705bc842c9a5141bb5b5
thanhtri2112 <thanhtri21120309@gmail.com>
2024-05-23 18:42:49 +0700

    code xml DangNhapOTP

---
+++ Chat App/app/src/main/java/thanhtri63131548/ntuedu/chatapp/DangNhapOTP.java
@@ -0,0 +1,14 @@
+package thanhtri63131548.ntuedu.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DangNhapOTP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dangnhapotp);
+    }
+}
