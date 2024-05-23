59b7dfb9a5a657f873f756c8699346636fecc3ac
thanhtri2112 <thanhtri21120309@gmail.com>
2024-05-23 18:21:45 +0700

    code màn hình đăng nhập bằng sđt

--- Chat App/app/src/main/java/thanhtri63131548/ntuedu/chatapp/ManHinhGioiThieu.java
+++ Chat App/app/src/main/java/thanhtri63131548/ntuedu/chatapp/ManHinhGioiThieu.java
@@ -2,7 +2,9 @@ package thanhtri63131548.ntuedu.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class ManHinhGioiThieu extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class ManHinhGioiThieu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manhinhgioithieu);
+        new Handler().postDelayed(new Runnable(){
+                @Override
+                public void run(){
+                    startActivity(new Intent(ManHinhGioiThieu.this,DangNhapSDT.class));
+                    finish();
+                }
+        },3000);
     }
 }
