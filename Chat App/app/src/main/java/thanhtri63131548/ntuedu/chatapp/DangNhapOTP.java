5deb5cf5ff6298a7c608df9e0629a98f41fdb2b9
thanhtri2112 <thanhtri21120309@gmail.com>
2024-05-24 16:57:32 +0700

    hoàn thiện xác thực otp và gửi lại otp

--- Chat App/app/src/main/java/thanhtri63131548/ntuedu/chatapp/DangNhapOTP.java
+++ Chat App/app/src/main/java/thanhtri63131548/ntuedu/chatapp/DangNhapOTP.java
@@ -3,6 +3,7 @@ package thanhtri63131548.ntuedu.chatapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,8 +12,11 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.Firebase;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
@@ -21,6 +25,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 import thanhtri63131548.ntuedu.chatapp.utils.AndroidUtil;
@@ -48,9 +54,23 @@ public class DangNhapOTP extends AppCompatActivity {
         guilaiOtpTv = findViewById(R.id.resend_otp_textview);
 
         sdt = getIntent().getExtras().getString("dt");
+
         guiOTP(sdt, false);
+
+        tieptheoBtn.setOnClickListener(v->{
+            String nhapOTP = otpInput.getText().toString();
+            PhoneAuthCredential credential = PhoneAuthProvider.getCredential(xacthucCode,nhapOTP);
+            dangNhap(credential);
+            setInProgress(true);
+        });
+
+        guilaiOtpTv.setOnClickListener((v) -> {
+            guiOTP(sdt,true);
+        });
+
     }
     void guiOTP (String sdt,boolean isResend){
+        demtgGuilaiOTP();
         setInProgress(true);
         PhoneAuthOptions.Builder builder =
                 PhoneAuthOptions.newBuilder(mAuth)
@@ -97,6 +117,37 @@ public class DangNhapOTP extends AppCompatActivity {
         }
     }
     void dangNhap(PhoneAuthCredential phoneAuthCredential){
-
+        setInProgress(true);
+        mAuth.signInWithCredential(phoneAuthCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                setInProgress(false);
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(DangNhapOTP.this,DangNhapUsername.class);
+                    intent.putExtra("dt",sdt);
+                    startActivity(intent);
+                }else{
+                    AndroidUtil.hienthiToast(getApplicationContext(),"Xác thực OTP thất bại");
+                }
+            }
+        });
+    }
+    void demtgGuilaiOTP(){
+        guilaiOtpTv.setEnabled(false);
+        Timer timer =  new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                timeoutSecond--;
+                guilaiOtpTv.setText("Gửi lại OTP trong "+timeoutSecond+" giây");
+                if(timeoutSecond<=0){
+                    timeoutSecond=60L;
+                    timer.cancel();
+                    runOnUiThread(() -> {
+                        guilaiOtpTv.setEnabled(true);
+                    });
+                }
+            }
+        },0,1000);
     }
 }
